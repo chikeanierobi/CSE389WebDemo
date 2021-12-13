9bf1bfb123e17de5adda574fbe6869db55d6857a
chikeanierobi <95322855+chikeanierobi@users.noreply.github.com>
2021-12-13 13:00:32 -0500

    Update RegistrationServlet.java

--- RegistrationServlet.java
+++ RegistrationServlet.java
@@ -1,6 +1,5 @@
 package com.ServletPackage;
-import java.io.File;
-import java.io.FileWriter;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
@@ -15,38 +14,35 @@ public class RegistrationServlet extends jakarta.servlet.http.HttpServlet{
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;  //universal version identifier for a Serializable class
 	
 	public RegistrationServlet() {
-		super();
+		super();  //Reference parent class objects
 	}
-	
+	//Create variables for the input that will be put in by the user
 	String uname;
 	String uemail;
 	String upassword1;
 	String upassword2;
+	String uphone;
+	String dateOfBirth;
+	//
 	public void doPost(HttpServletRequest req1, HttpServletResponse res1) throws IOException {
 		res1.setContentType("text/html");
 		uname = req1.getParameter("username");
 		uemail = req1.getParameter("useremail");
 		upassword1 = req1.getParameter("password1");
-		upassword2 = req1 .getParameter("password2");
+		upassword2 = req1.getParameter("password2");
+		uphone = req1.getParameter("phone");
+		dateOfBirth = req1.getParameter("birthdate");
 		res1.sendRedirect("WebDemo.jsp");
-		//PrintWriter out = res1.getWriter();
+		PrintWriter out = res1.getWriter();
 ////This would only work if the Servlet is registered in web.xml
-//		out.println(uname);
-//		out.println(uemail);
-//		out.println(upassword1);
-//		out.println(upassword2);
-// 		File file1 = new File("UserRegistrationDatabase.txt");
-		
-// 		FileWriter fw = new FileWriter(file1);
-		
-// 		PrintWriter pw = new PrintWriter(fw);
-		
-// 		pw.print(uname + " " + uemail + " " + upassword1 + " " + upassword2);
-		
-// 		pw.close();
+		out.println(uname);
+		out.println(uemail);
+		out.println(upassword1);
+		out.println(upassword2);
+
 		
 //		try {
 //			Class.forName("com.mysql.jdbc.Driver");
