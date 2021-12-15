6e631f29c3b56f4ce7af36d6032f44e753c2f894
abbe42 <59491649+abbe42@users.noreply.github.com>
2021-12-15 17:31:59 -0500

    Update LoginServlet.java

--- LoginServlet.java
+++ LoginServlet.java
@@ -1,18 +1,20 @@
 package com.ServletPackage;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-public class LoginServlet extends HttpServlet {
-	
+public class LoginServlet extends javax.servlet.http.HttpServlet {	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	String email;
 	String password;
 	
@@ -27,20 +29,29 @@ public class LoginServlet extends HttpServlet {
 		}else {
 			res1.sendRedirect("Error.jsp");
 		}
-//		try {
-//			Class.forName("com.mysql.jdbc.Driver");
-//			Connection con1 = DriverManager.getConnection("jdbc:mysql://localhost/database1","root","");
-//			Statement stmnt = con1.createStatement();
-//			String query = "select * from users where useremail = '" + email + "' and userpass1= '" + password + "'"; //You have to check that it's equal to what is in the database
-//			ResultSet rs1 = stmnt.executeQuery(query);
-//			
-//			if (rs1.next()) {  //We want to check if it matches what we have in the database, so login would be successful
-//				out.println("Logged in successfully!");
-//			}else {
-//				out.println("Login failed!");
-//			}
-//		}catch(Exception e) {
-//			out.println(e);
-//		}
+		try {
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection con1 = DriverManager.getConnection("jdbc:mysql://localhost:3306/login","root","Dimmy42!2");
+		Statement stmnt = con1.createStatement();
+		PreparedStatement preparedStatement=con1.prepareStatement("insert into base values(?,?,?)");
+		 System.out.println("reached");
+		 //Setting values for Each Parameter
+		preparedStatement.setString(1,email);
+        preparedStatement.setString(2,password);
+        preparedStatement.setString(3,email);
+        preparedStatement.executeUpdate();
+        String query = "select * from base where email = '" + email + "' and password= '" + password + "'"; //You have to check that it's equal to what is in the database
+		ResultSet rs1 = stmnt.executeQuery(query);
+   
+		System.out.println("reached");
+		
+		if (rs1.next()) {  //We want to check if it matches what we have in the database, so login would be successful
+			System.out.println("Logged in successfully!");
+		}else {
+			System.out.println("Login failed!");
+		}
+	}catch(Exception e) {
+		System.out.println(e);
+	}
 	}
 }
